62370b77a1e2ac26442deedfedf31d5a63ebf19c
Nicholas Brown <nb@brownsfromsa.com>
2022-03-24 23:50:29 -0400

    Added the content for JavaScript, CSS, & HTML

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     let leftButton = document.getElementById(`left-button`),
         rightButton = document.getElementById(`right-button`),
+        textBox = document.getElementById(`text-box`),
         errorStyles = 
         `background-color: red;
         padding: 2px;
@@ -12,12 +13,32 @@ window.onload = () => {
         font-family: inherit;
         border-radius: none`;
 
-    function showAlert () {
-        alert(`JavaScript Works`);
+    function showWhiteRecipe () {
+        textBox.innerHTML = 
+        `Step 1: Combine 1 cup of rice with 2 cups of water and
+        1 Tbsp olive oil. <br> Step 2: Bring to a boil,
+        then reduce heat to the lowest setting. <br> 
+        Step 3: Cook for about 18 minutes.`;
+    }
+
+    function showCaliRecipe () {
+        textBox.innerHTML = 
+        `For slightly al dente rice:<br>
+        Step 1: Combine 1 1/4 cups of rice with 2 cups of water or 
+        broth and 1 Tbsp olive oil. <br> 
+        Step 2: Bring to a boil and stir once to mix. <br>
+        Step 3: Reduce heat to low, cover with a tight-fitting lid 
+        and cook for 25 minutes. <br>
+        Step 4: Remove from heat and let stand for 5 minutes. <br> 
+        Step 5: Fluff with a fork and serve. <br><br>
+        
+        For softer rice: <br>
+        Increase liquid by 1/2 cup and cook time by 5 minutes.`;    
     }
 
     if (null !== leftButton) {
-        leftButton.addEventListener(`click`, showAlert);
+        leftButton.addEventListener(`click`, showWhiteRecipe);
+  
     } else {
         console.error(
             `A reference to ID %c left-button %c could not be established`, 
@@ -27,7 +48,7 @@ window.onload = () => {
     }
 
     if (null !== rightButton) {
-        rightButton.addEventListener(`click`, showAlert);
+        rightButton.addEventListener(`click`, showCaliRecipe);
     } else {
         console.error(
             `A reference to ID %c right-button %c could not be established`, 
